1350cf2b9de262362d5d5ac05fee1a3b64ef1e7c
pandaec <ericchan1026@gmail.com>
2019-07-24 17:20:21 +0800

    Updates

---
+++ precache-manifest.e2cd1ff53146fd2d3ad023252bc11dc6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bf53bf390d268754f9bc735e89baf74",
+    "url": "/hnews/index.html"
+  },
+  {
+    "revision": "39b69dd90ed4042ad96f",
+    "url": "/hnews/static/css/main.f7e75901.chunk.css"
+  },
+  {
+    "revision": "5adfce6a37237f5d7eb0",
+    "url": "/hnews/static/js/2.48afb5d8.chunk.js"
+  },
+  {
+    "revision": "39b69dd90ed4042ad96f",
+    "url": "/hnews/static/js/main.5270dd7c.chunk.js"
+  },
+  {
+    "revision": "f4607bbc5d066e4d7ec3",
+    "url": "/hnews/static/js/runtime~main.ffd017e3.js"
+  }
+]);
