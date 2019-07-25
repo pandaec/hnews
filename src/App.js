3028ea1049719fdd39469863621ab20d8755471a
pandaec <ericchan1026@gmail.com>
2019-07-25 16:47:38 +0800

    add back button on navbar

--- src/App.js
+++ src/App.js
@@ -1,28 +1,41 @@
 import React from 'react';
-import { BrowserRouter as HashRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 import Home from './components/Home';
 import Post from './components/Post';
 import './App.scss';
 
-function App() {
-  return (
-    <HashRouter basename={process.env.PUBLIC_URL}>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to='/'>HNews</Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-      <Route path="/" exact component={Home} />
-      <Route path="/post/:id" component={Post} />
+    this.state = {
+      title: 'HW news'
+    }
+  }
 
-    </HashRouter>
-  );
+  render() {
+    return (
+      <Router basename={process.env.PUBLIC_URL}>
+        <div>
+          <nav>
+            <div className="nav-container">
+              <div className="nav-back"><i className="fa fa-long-arrow-left"></i></div>
+              <div className="nav-title">
+                <span>
+                  {this.state.title}
+                </span>
+              </div>
+            </div>
+          </nav>
+        </div>
+
+        <Route path="/" exact component={Home} />
+        <Route path="/post/:id" component={Post} />
+
+      </Router>
+    );
+  }
 }
 
 export default App;
