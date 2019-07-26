06430d575efa242b95703a29ab6bbe70fa97aab6
pandaec <ericchan1026@gmail.com>
2019-07-26 16:35:32 +0800

    clicking title in home will back to first page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,26 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 function NavBar(props) {
     return (
         <div>
             <nav>
                 <div className="nav-container">
-                    { props.history ? 
-                        <div className="nav-back" onClick={()=>props.history.goBack()}><i className="fa fa-long-arrow-left"></i></div>
+                    {props.history ?
+                        <div className="nav-back" onClick={() => props.history.goBack()}><i className="fa fa-long-arrow-left"></i></div>
                         :
                         ''
                     }
                     <div className="nav-title">
-                        <span>
-                            {props.title}
-                        </span>
+                        {props.history ?
+                            <span>
+                                {props.title}
+                            </span>
+                            :
+                            <Link to={'/'}>
+                                {props.title}
+                            </Link>
+                        }
                     </div>
                 </div>
             </nav>
